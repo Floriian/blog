94c42ee92ef5b2886051ed9ed94958702ac05ba9
Floriian <lovasifloriian@outlook.hu>
2023-05-14 22:24:10 +0200

    api: add status codes, and apitag

--- backend/src/comments/comments.controller.ts
+++ backend/src/comments/comments.controller.ts
@@ -1,27 +1,42 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/CreateComment.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('comments')
+@ApiTags('Comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   getPostComments(@Param('id') id: string) {
     return this.commentsService.getPostComments(id);
   }
 
   @Post(':id')
+  @HttpCode(HttpStatus.CREATED)
   createComment(@Param('id') id: string, @Body() dto: CreateCommentDto) {
     return this.commentsService.createComment(id, dto);
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.ACCEPTED)
   deleteComment(@Param('id') id: string) {
     return this.commentsService.deleteComment(id);
   }
 
   @Delete('/all/:id')
+  @HttpCode(HttpStatus.ACCEPTED)
   deleteAllComment(@Param('id') id: string) {
     return this.commentsService.deleteAll(id);
   }
