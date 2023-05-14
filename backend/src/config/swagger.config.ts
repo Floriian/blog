bb534f0ee1f560bc52c862899e52e31bcd9f4bed
Floriian <lovasifloriian@outlook.hu>
2023-05-14 22:24:47 +0200

    api: remove tag

--- backend/src/config/swagger.config.ts
+++ backend/src/config/swagger.config.ts
@@ -4,5 +4,4 @@ export const swaggerConfig = new DocumentBuilder()
   .setTitle('Blog Backend API')
   .setDescription('API documentation for blog')
   .setVersion('1.0')
-  .addTag('blog')
   .build();
