fac5f3586015bbabfbb36ed9beb54dd3aec07029
Floriian <lovasifloriian@outlook.hu>
2023-05-14 12:50:06 +0200

    api: generate comments.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { mongooseOptions } from './config/mongoose';
 import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync(mongooseOptions),
     PostsModule,
+    CommentsModule,
   ],
 })
 export class AppModule {}
