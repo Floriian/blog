f346da864b71cc2293d554fed43417382a0af7ce
Floriian <lovasifloriian@outlook.hu>
2023-05-17 00:00:48 +0200

    client: basic comment component.

--- frontend/src/components/Comment/Comment.tsx
+++ frontend/src/components/Comment/Comment.tsx
@@ -1,8 +1,18 @@
 import { Comment } from "../../types/comment";
+import IconDislike from "../Icons/IconDislike";
+import IconLike from "../Icons/IconLike";
 
 type Props = {
   comment: Comment;
 };
 export default function Comment({ comment }: Props) {
-  return <div className="border-2 border-gray-500"></div>;
+  return (
+    <div className="border-2 border-gray-500 p-1 rounded-md">
+      {comment.content}
+      <div className="flex justify-end items-center">
+        {comment.likes < 0 ? <IconLike /> : <IconDislike />}
+        {comment.likes}
+      </div>
+    </div>
+  );
 }
