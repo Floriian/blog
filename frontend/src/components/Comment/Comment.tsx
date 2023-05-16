d1823e6dc8588bbc231e4dc771165dc86e3ae300
Floriian <lovasifloriian@outlook.hu>
2023-05-17 00:07:17 +0200

    client: add comment like animation

--- frontend/src/components/Comment/Comment.tsx
+++ frontend/src/components/Comment/Comment.tsx
@@ -9,8 +9,12 @@ export default function Comment({ comment }: Props) {
   return (
     <div className="border-2 border-gray-500 p-1 rounded-md">
       {comment.content}
-      <div className="flex justify-end items-center">
-        {comment.likes >= 0 ? <IconLike /> : <IconDislike />}
+      <div className="flex justify-end items-center gap-2">
+        {comment.likes >= 0 ? (
+          <IconLike className="cursor-pointer hover:text-blue-500 transition-colors duration-150" />
+        ) : (
+          <IconDislike className="cursor-pointer hover:text-blue-500 transition-colors duration-150" />
+        )}
         {comment.likes}
       </div>
     </div>
