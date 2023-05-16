408a96ecc26b13bbca2c427064f8ceab3ece1710
Floriian <lovasifloriian@outlook.hu>
2023-05-16 16:39:30 +0200

    client: add styling to blogpreview

--- frontend/src/components/Blog/BlogPreview.tsx
+++ frontend/src/components/Blog/BlogPreview.tsx
@@ -1,15 +1,33 @@
 import { Post } from "../../types/post";
+import IconComment from "../Icons/IconComment";
+import IconLike from "../Icons/IconLike";
 
 type Props = {
   post: Post;
 };
 export default function BlogPreview({ post }: Props) {
   return (
-    <div className="border border-gray-500 max-w-lg rounded m-2">
+    <section className="border border-gray-500 max-w-lg w-full rounded m-2 p-2">
       <h1 className="font-bold text-2xl">{post.title}</h1>
-      {post.content}
-      {post.likes}
-      Comments {post.comments}
-    </div>
+      <p className="text-sm text-gray-500">
+        {post.createdAt.toString().split("T")[0]}
+      </p>
+      <hr />
+      <p>
+        {post.content.length > 100
+          ? post.content.slice(0, 100) + "..."
+          : post.content}
+      </p>
+      <div className="flex justify-end gap-2 text-lg">
+        <div className="flex items-center cursor-pointer">
+          <IconComment className="hover:text-blue-600 transition-colors duration-150" />
+          {post.comments}
+        </div>
+        <div className="flex items-center cursor-pointer">
+          <IconLike className="hover:text-blue-600 transition-colors duration-150" />
+          {post.likes}
+        </div>
+      </div>
+    </section>
   );
 }
